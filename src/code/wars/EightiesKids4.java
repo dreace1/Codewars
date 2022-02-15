5ffa476482126c9caf97cbea4369821194e49802
Hannes <hannesro27@gmail.com>
2022-02-15 11:42:25 +0100

    EightieKids4 not finished

---
+++ src/code/wars/EightiesKids4.java
@@ -0,0 +1,28 @@
+package code.wars;
+
+/**
+ * https://www.codewars.com/kata/56648a2e2c464b8c030000bf/train/java
+ *
+ * @author rosen
+ *
+ */
+public class EightiesKids4 {
+
+	public static String markSpot(float n) {
+		if (n % 2 == 0 || n < 1) {
+			return "?";
+		}
+
+		String markedSpot = "";
+
+		if (n == 1) {
+			markedSpot = "X\n";
+		} else {
+			for (int i = 0; i < n; i++) {
+
+			}
+		}
+		return "";
+
+	}
+}
